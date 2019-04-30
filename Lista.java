4f1bc0fd5eedd471115f2283262e4acbcd438862
Breno Amaral <26623301+BrAmaral@users.noreply.github.com>
2019-04-30 11:58:15 +0000

    TUTUTUTUTUUUTUTUTUTUTUUTUTUTUUTUTUAGIUAGUAITTGIWBF

--- Lista.java
+++ Lista.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  */
 public class Lista //implements IArmazenador
 {
-    private ArrayList arrayL;
+    private ArrayList<Object> arrayL = new ArrayList<Object>();
     private int qtdd;
     
     public Lista(){
@@ -30,4 +30,31 @@ public class Lista //implements IArmazenador
     public void setQtdd(int qtdd){
         this.qtdd = qtdd;
     } 
+    
+    public void adicionar(Object x){
+        arrayL.add(x);
+        setQtdd(getQtdd()+1);
+    }
+    
+    public void remover(Object x){
+        arrayL.remove(x);
+        setQtdd(getQtdd()-1);
+    }
+    
+    public int tamanho(Object x){
+        return arrayL.size();
+    }
+    
+    public Object buscar(int x){
+        return arrayL.get(x);
+    }
+    
+    public boolean vazia(){
+        return (qtdd == 0);
+    }
+    
+    public void imprimir(){
+        System.out.println(arrayL);
+    }
+    
 }
---
+++ teste2.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class teste2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class teste2
+{
+    public static void main(String args[]){
+        Lista objeto = new Lista();
+        
+        objeto.adicionar(1);
+        objeto.adicionar(2);
+        objeto.imprimir();
+    }
+}
